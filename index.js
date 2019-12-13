c707be3f480b400c5b4a87a06aa24a7ddbd27440
annazus <suzanna@appliedar.us>
2019-12-12 17:11:28 -0800

    Removed example code

--- index.js
+++ index.js
@@ -7,38 +7,3 @@ exports.handler = async (event, context, callback) => {
   await scrape(process.env.BUCKET_NAME);
   callback(null, "Finished");
 };
-// (async () => {
-//   await scrape();
-// })();
-// const chromium = require("chrome-aws-lambda");
-// const clipperRaceStandingsUrl =
-//   "https://www.clipperroundtheworld.com/race/standings";
-
-// exports.handler = async (event, context) => {
-//   let result = null;
-//   let browser = null;
-
-//   try {
-//     browser = await chromium.puppeteer.launch({
-//       args: chromium.args,
-//       defaultViewport: chromium.defaultViewport,
-//       executablePath: await chromium.executablePath,
-//       headless: chromium.headless
-//     });
-
-//     let page = await browser.newPage();
-
-//     await page.goto(clipperRaceStandingsUrl);
-
-//     result = await page.title();
-//     console.log(result);
-//   } catch (error) {
-//     return context.fail(error);
-//   } finally {
-//     if (browser !== null) {
-//       await browser.close();
-//     }
-//   }
-
-//   return context.succeed(result);
-// };
